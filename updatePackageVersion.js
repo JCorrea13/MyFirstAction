bf80865567f5c73ef61f9b32de7fecec547bd5a9
Jose Correa <josemanuelcorrea.13@gmail.com>
2021-02-05 22:34:18 -0600

    fixing version

--- updatePackageVersion.js
+++ updatePackageVersion.js
@@ -1,15 +1,15 @@
 const semver = require('./semver');
 const fs = require('fs');
 
-const packageJsonPath = '/github/workspace/package.json';
+const packageJsonPath = './github/workspace/package.json';
 const packageJson = require(packageJsonPath);
 const versionToRelease = process.argv[2].toLowerCase();
 
 const currentVersion = packageJson.version.split('.');
 
-const major = versionToRelease == semver.levels.Major ? currentVersion[0] + 1 : currentVersion[0];
-const minor = versionToRelease == semver.levels.Minor ? currentVersion[1] + 1 : currentVersion[1];
-const patch = versionToRelease == semver.levels.Patch ? currentVersion[2] + 1 : currentVersion[2];
+const major = versionToRelease == semver.levels.Major ? parseInt(currentVersion[0]) + 1 : currentVersion[0];
+const minor = versionToRelease == semver.levels.Minor ? parseInt(currentVersion[1]) + 1 : currentVersion[1];
+const patch = versionToRelease == semver.levels.Patch ? parseInt(currentVersion[2]) + 1 : currentVersion[2];
 
 packageJson.version = `${major}.${minor}.${patch}`;
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, undefined, 4));
