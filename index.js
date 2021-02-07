b39d675ff1fdca4c5dbc60d0996e51c3e60f878a
Jose Correa <josemanuelcorrea.13@gmail.com>
2021-02-07 13:52:57 -0600

    pull bf adding changes

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const ops = require('./jsonOperations');
 const actionType = core.getInput('action-type');
 const git = simpleGit();
 
-const newVersion = ops.updateVersion(actionType);
 ( async () => {
     await git.addConfig('user.email', process.env.GITHUB_ACTOR, undefined);
     await git.addConfig('user.name', `${process.env.GITHUB_ACTOR}@users.noreply.github.com`, undefined);
-
+    
     await git.add('.');
     await git.pull('master');
+    
+    const newVersion = ops.updateVersion(actionType);
     await git.commit(`Releasing Version: ${newVersion}`);
     await git.push();
 })().then(() => {
