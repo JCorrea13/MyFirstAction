db2a57fed3a69653472fcd928ecab489b6ae6ab6
undefined@users.noreply.github.com <undefined>
2021-02-09 18:17:31 -0600

    adding tag and deleting branch

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const core = require('@actions/core');
-const simpleGit = require('simple-git');
 const ops = require('./jsonOperations');
 const github = require('@actions/github');
 
 const actionType = core.getInput('action-type');
 const token = core.getInput('github_token');
 const sprint = core.getInput('sprint');
+const releaseNotes = core.getInput('release-notes');
+
+
 const octokit = github.getOctokit(token);
 const repo = github.context.repo;
 
 const pushReleaseVersion = async () => {
     const newJson = ops.updateVersion(actionType, process.cwd());
+    const newRef = `refs/heads/Chore/Sprint${sprint}`;
 
     const masterBranch = await octokit.git.getRef({
         owner: repo.owner,
@@ -21,7 +24,7 @@ const pushReleaseVersion = async () => {
     await octokit.git.createRef({
         owner: repo.owner,
         repo: repo.repo,
-        ref: `refs/heads/Chore/Sprint${sprint}`,
+        ref: newRef,
         sha: masterBranch.data.object.sha
     });
 
@@ -37,7 +40,7 @@ const pushReleaseVersion = async () => {
         owner: repo.owner,
         repo: repo.repo,
         path: 'package.json',
-        ref: `refs/heads/Chore/Sprint${sprint}`
+        ref: newRef
     });
 
     await octokit.repos.createOrUpdateFileContents({
@@ -66,24 +69,31 @@ const pushReleaseVersion = async () => {
       title: `Chore/Sprint${sprint}`
     });
 
-    await octokit.pulls.merge({
+    const merge = await octokit.pulls.merge({
         owner: repo.owner,
         repo: repo.repo,
         pull_number: pr.data.number
     });
 
+    await octokit.git.deleteRef({
+        owner: repo.owner,
+        repo: repo.repo,
+        ref: newRef
+    });
+
+    await octokit.git.createTag({
+        owner: repo.owner,
+        repo: repo.repo,
+        object: merge.data.sha,
+        message: releaseNotes,
+        tag: `v${sprint}`,
+        type: 'commit'
+    });
+
     return newJson.version;
 };
 
-const configureGit = () => {
-    const git = simpleGit({ baseDir: process.cwd() });
-
-    return git.addConfig('user.email', process.env.GITHUB_ACTOR, undefined)
-    .then(() => git.addConfig('user.name', `${process.env.GITHUB_ACTOR}@users.noreply.github.com`, undefined));
-}
-
-configureGit()
-.then(pushReleaseVersion)
+pushReleaseVersion()
 .then((version) => {
     core.info(`Successfully Released Package Version: ${version}`);
 })
