228a6551c21e936296c061fefd1fb1419cab76a0
undefined@users.noreply.github.com <undefined>
2021-02-17 17:53:50 -0600

    refactor

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
-const ops = require('./jsonOperations');
 const github = require('@actions/github');
+const ops = require('./jsonOperations');
+const ghUtilities = require('./utils');
 
 const actionType = core.getInput('action-type');
 const token = core.getInput('github_token');
@@ -10,19 +11,23 @@ const prodBranch = core.getInput('prod-branch');
 
 const octokit = github.getOctokit(token);
 const repo = github.context.repo;
+const gh = ghUtilities.getUtilities(octokit, repo, process);
 
 const pushReleaseVersion = async () => {
     const newJson = ops.updateVersion(actionType, process.cwd());
-    const newRef = `refs/heads/Chore/Sprint${sprint}`;
+    const newBranchName = `Chore/Sprint${sprint}`;
+    const newRef = `refs/heads/${newBranchName}`;
 
-    const repository = await octokit.repos.get({
+    const defaultBranchName = await gh.getDefaultBranch();
+    /*const repository = await octokit.repos.get({
         owner: repo.owner,
         repo: repo.repo
     });
 
-    const defaultBranchName = repository.data.default_branch;
+    const defaultBranchName = repository.data.default_branch;*/
 
-    const masterBranch = await octokit.git.getRef({
+    await gh.createNewBranch(prodBranch, newRef);
+    /*const masterBranch = await octokit.git.getRef({
         owner: repo.owner,
         repo: repo.repo,
         ref: `heads/${prodBranch}`
@@ -33,30 +38,38 @@ const pushReleaseVersion = async () => {
         repo: repo.repo,
         ref: newRef,
         sha: masterBranch.data.object.sha
-    });
+    });*/
 
-    await octokit.repos.merge({
+    await gh.mergeBranches(newBranchName, defaultBranchName);
+    /*await octokit.repos.merge({
         owner: repo.owner,
         repo: repo.repo,
-        base: `Chore/Sprint${sprint}`,
+        base: newBranchName,
         head: defaultBranchName,
         commit_message: `Merging ${defaultBranchName}`
-    });
+    });*/
     
-    const packageJson = await octokit.repos.getContent({ 
+    const packageJson = await gh.getContent(newRef, 'package.json');
+    /*const packageJson = await octokit.repos.getContent({ 
         owner: repo.owner,
         repo: repo.repo,
         path: 'package.json',
         ref: newRef
-    });
+    });*/
 
-    await octokit.repos.createOrUpdateFileContents({
+    await gh.commitContent(
+        'package.json',
+        `Updating Package Version to ${newJson.version}`,
+        Buffer.from(JSON.stringify(newJson, undefined, 4)).toString('base64'),
+        packageJson.sha,
+        newBranchName);
+    /*await octokit.repos.createOrUpdateFileContents({
         owner: repo.owner,
         repo: repo.repo,
         path: 'package.json',
         message: `Updating Package Version to ${newJson.version}`,
         content: Buffer.from(JSON.stringify(newJson, undefined, 4)).toString('base64'),
-        sha: packageJson.data.sha,
+        sha: packageJson.sha,
         committer: {
             name: process.env.GITHUB_ACTOR,
             email: `${process.env.GITHUB_ACTOR}@users.noreply.github.com`,
@@ -65,30 +78,33 @@ const pushReleaseVersion = async () => {
             name: process.env.GITHUB_ACTOR,
             email: `${process.env.GITHUB_ACTOR}@users.noreply.github.com`,
         },
-        branch: `Chore/Sprint${sprint}`
-    });
+        branch: newBranchName
+    });*/
 
-    const pr = await octokit.pulls.create({
+    const merge = await gh.createAndMergePR(prodBranch, newBranchName);
+    /*const pr = await octokit.pulls.create({
       owner: repo.owner,
       repo: repo.repo,
-      head: `Chore/Sprint${sprint}`,
+      head: newBranchName,
       base: prodBranch,
-      title: `Chore/Sprint${sprint}`
+      title: newBranchName
     });
 
     const merge = await octokit.pulls.merge({
         owner: repo.owner,
         repo: repo.repo,
         pull_number: pr.data.number
-    });
+    });*/
 
-    await octokit.git.deleteRef({
+    await gh.deleteBranch(newBranchName);
+    /*await octokit.git.deleteRef({
         owner: repo.owner,
         repo: repo.repo,
-        ref: `heads/Chore/Sprint${sprint}`
-    });
+        ref: `heads/${newBranchName}`
+    });*/
 
-    const tag = await octokit.git.createTag({
+    await gh.createTag(merge.sha, sprint, releaseNotes);
+    /*const tag = await octokit.git.createTag({
         owner: repo.owner,
         repo: repo.repo,
         object: merge.data.sha,
@@ -97,7 +113,7 @@ const pushReleaseVersion = async () => {
         type: 'commit',
     });
 
-    const tagRef = await octokit.git.createRef({
+    await octokit.git.createRef({
         owner: repo.owner,
         repo: repo.repo,
         sha: tag.data.object.sha,
@@ -110,7 +126,7 @@ const pushReleaseVersion = async () => {
         tag_name: sprint,
         name: `Release ${sprint}`,
         body: releaseNotes
-    });
+    });*/
 
     return newJson.version;
 };
